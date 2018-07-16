934d8f0c41efa0dbb207d83d4e09407f524cf473
tangdf <tangdf@users.noreply.github.com>
2018-07-16 12:08:36 +0800

    EF and EF Core Guid Generation

---
+++ test/Abp.EntityFramework.Tests/AbpDbContext_PrimaryKey_Guid_Generation.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Abp.EntityFramework.Tests
+{
+    public class AbpDbContext_PrimaryKey_Guid_Generation
+    {
+
+        [Fact]
+        public void Entity_Create() {
+
+            Database.SetInitializer<TestDbContext>(new DropCreateDatabaseIfModelChanges<TestDbContext>());
+
+
+            var sqlConnectionStringBuilder = new SqlConnectionStringBuilder {
+                DataSource = "*****",
+                InitialCatalog = "abpEF",
+                UserID = "sa",
+                Password = "******"
+            };
+
+            using (TestDbContext testDbContext = new TestDbContext(sqlConnectionStringBuilder.ToString())) {
+                var guid = Guid.NewGuid();
+
+                var testGuidGenerator = new TestGuidGenerator(guid);
+                testDbContext.GuidGenerator = testGuidGenerator;
+
+                var entity = testDbContext.Set<GuidEntity>().Add(new GuidEntity {
+                    Number = 1
+                });
+                testDbContext.SaveChanges();
+
+                Assert.True(testGuidGenerator.CreateCalled);
+                Assert.Equal(guid, entity.Id);
+            }
+        }
+
+
+        public class TestGuidGenerator : IGuidGenerator
+        {
+            private Guid _guid;
+
+            public TestGuidGenerator(Guid guid) {
+                _guid = guid;
+            }
+
+            public Guid Create() {
+                CreateCalled = true;
+                return _guid;
+            }
+
+            public bool CreateCalled { get; private set; }
+        }
+
+        public class TestDbContext : AbpDbContext
+        {
+            public TestDbContext(string nameOrConnectionString) : base(nameOrConnectionString) {
+            }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+                modelBuilder.Entity<GuidEntity>().ToTable("GuidTable");
+                modelBuilder.Entity<GuidEntity>().Property(e => e.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+
+        public class GuidEntity : Entity<Guid>
+        {
+            public override Guid Id { get; set; }
+            public int Number { get; set; }
+        }
+    }
+}
---
+++ test/Abp.EntityFrameworkCore.Tests/AbpDbContext_PrimaryKey_Guid_Generation.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlClient;
+using Abp.Domain.Entities;
+using Abp.EntityFrameworkCore.Tests.Ef;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Abp.EntityFrameworkCore.Tests
+{
+    public class AbpDbContext_PrimaryKey_Guid_Generation
+    {
+
+        [Fact]
+        public void Entity_Create() {
+
+            var sqlConnectionStringBuilder = new SqlConnectionStringBuilder {
+                DataSource = "*****",
+                InitialCatalog = "abpDfCore",
+                UserID = "sa",
+                Password = "*****"
+            };
+
+            var builder = new DbContextOptionsBuilder<TestDbContext>();
+
+
+            builder.UseSqlServer(sqlConnectionStringBuilder.ToString());
+
+
+            using (TestDbContext testDbContext = new TestDbContext(builder.Options)) {
+
+                testDbContext.Database.EnsureDeleted();
+                testDbContext.Database.EnsureCreated();
+
+                var guid = Guid.NewGuid();
+
+                var testGuidGenerator = new TestGuidGenerator(guid);
+
+                testDbContext.GuidGenerator = testGuidGenerator;
+
+                var entity = new GuidEntity {
+                    Number = 1
+                };
+
+                testDbContext.Set<GuidEntity>().Add(entity);
+
+                testDbContext.SaveChanges();
+
+                Assert.True(testGuidGenerator.CreateCalled);
+
+                Assert.Equal(guid, entity.Id);
+            }
+        }
+
+
+        public class TestGuidGenerator : IGuidGenerator
+        {
+            private Guid _guid;
+
+            public TestGuidGenerator(Guid guid) {
+                _guid = guid;
+            }
+
+            public Guid Create() {
+                CreateCalled = true;
+                return _guid;
+            }
+
+            public bool CreateCalled { get; private set; }
+        }
+
+        public class TestDbContext : AbpDbContext
+        {
+            public TestDbContext(DbContextOptions options) : base(options) {
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder) {
+                modelBuilder.Entity<GuidEntity>().ToTable("GuidTable");
+                modelBuilder.Entity<GuidEntity>().Property(e => e.Id).ValueGeneratedOnAdd();
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+
+        public class GuidEntity : Entity<Guid>
+        {
+            public override Guid Id { get; set; }
+            public int Number { get; set; }
+        }
+    }
+}
